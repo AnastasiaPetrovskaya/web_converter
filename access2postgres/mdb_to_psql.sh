ab7f988822d906c0aefa8579b451cc1af0249aa8
rofl3228 <kir.mbr@gmail.com>
2019-10-10 00:41:53 +0300

    from server

--- access2postgres/mdb_to_psql.sh
+++ access2postgres/mdb_to_psql.sh
@@ -5,7 +5,7 @@ createdb $2
 
 mdb-tables $1
 
-mdb-schema $1 postgres | psql -d $2 -U test -h localhost
+mdb-schema $1 postgres | psql -d $2 -U converter -h localhost
 
 mdb-tables -1 Test.mdb| while read TT
 do
