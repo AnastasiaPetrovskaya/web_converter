2555e9d4102540a7634a4a12df094e58a944c7cb
AnastasiaPetrovskaya <ap@MacBook-Pro-Anastasia-2.local>
2019-11-08 18:10:56 +0300

    fixes

--- test/test_students.js
+++ test/test_students.js
@@ -2,17 +2,11 @@
 var RelationalAlgebraQuery = require('../lib/RelationalAlgebraQuery.js');
 
 var assert = require('assert');
-/*describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
-*/
 
-describe('Algebra', function() {
-    describe('proection transformation', function() {
+describe('Algebra Answers from students', function() {
+
+    describe('answers2019', function() {
+
         it('test1', function(done) {
             var query = new RelationalAlgebraQuery({
                 title: "test",
@@ -28,68 +22,71 @@ describe('Algebra', function() {
 
             query.convert()
                 .then(function(res) {
-                    //console.log("res", res);
-                    //console.log('query', query);
 
-                    assert.equal(query.query_body, "X[X.Nгр = Y.Nгр]Y");
-                    assert.equal(res, "X.Nгр, X.Фио, Y.Спец");
+                    assert.equal(query.query_body, '((Z*U*Y*W*X)[Z.ИдАктера=U.ИдАктераANDZ.Имя="Скарлеттsecret235Йоханссон"ANDZ.ИдФильма=Y.ИдФильмаANDY.ИдФильма=W.ИдФильмаANDW.ИдКинотеатра=X.ИдКинотеатра])');
                     done();
                 }).catch(function(err) {
                     done(err);
-                    //console.log('err', err);
                 });
         });
-
 
         it('test2', function(done) {
             var query = new RelationalAlgebraQuery({
                 title: "test",
-                alias: "Кинотеатры AS K, Фильмы AS F, ФильмыКинотеатры AS X",
-                target_list: "K.НазвКинотеатра, K.Метро, F.Название, X.ЦенаБилета",
-                query_body: '((' +
-                    '(K[K.Метро="Университет"])[K.ИдКинотеатра])' +
-                        'INTERSECT' +
-                    '(F[F.ИдКинотеатра])' +
-                    ')'
+                alias: "ФильмыВинДизель AS X, ФильмыДжейсонСтейтем AS Y",
+                target_list: "Y.Название, Y.Режиссер, Y.Год",
+                query_body: '(X[X.ИдФильма=Y.ИдФильма)Y)'
             });
 
             query.convert()
                 .then(function(res) {
-                    //console.log("res", res);
-                    //console.log('query', query);
 
-                    assert.equal(query.query_body, "X[X.Nгр = Y.Nгр]Y");
-                    assert.equal(res, "X.Nгр, X.Фио, Y.Спец");
-                    done();
+                    done({"message":"Expected error"});
+
                 }).catch(function(err) {
-                    done(err);
-                    //console.log('err', err);
+
+                    assert.equal(err.message, "После эквивалентных преобразований запрос не соответсвует необходимому шаблону");
+                    done();
                 });
         });
 
         it('test3', function(done) {
             var query = new RelationalAlgebraQuery({
                 title: "test",
-                alias: "Кинотеатры AS X, R AS T, R AS Y",
-                target_list: "X.НазвКинотеатра, X.Метро",
-                query_body: '(' +
-                    '(T[T.ИдКинотеатра=Y.ИдКинотеатра AND T.Название="Форсаж 1" AND Y.Название="Терминатор 1"]Y)' +
-                    '[T.ИдКинотеатра=X.ИдКинотеатра](X)' +
-                    ')'
+                alias: "Кинотеатры AS Z, ФильмыКинотеатры AS X, ФильмыКинотеатры AS Y, ФильмыКинотеатры AS U",
+                target_list: "Z.*",
+                query_body: '((Z[X.ИдКинотеатра = Z.ИдКинотеатра]X) EXCEPT ((((Z[X.ИдКинотеатра = Z.ИдКинотеатра]X)[ X.ИдКинотеатра = Y.ИдКинотеатра AND X.ИдФильма<>Y.ИдФильма]Y)[  Y.ИдКинотеатра = U.ИдКинотеатра AND Y.ИдФильма<>U.ИдФильма AND X.ИдФильма<>U.ИдФильма]U))?'
             });
 
             query.convert()
                 .then(function(res) {
-                    
-                    assert.equal(query.query_body, '((T*Y*X)[T.ИдКинотеатра=Y.ИдКинотеатраANDT.Название="Форсажsecret2351"ANDY.Название="Терминаторsecret2351"ANDT.ИдКинотеатра=X.ИдКинотеатра])');
-                    done();
+
+                    done({"message":"Expected error"});
                 }).catch(function(err) {
-                    done(err);
-                    //console.log('err', err);
+
+                    assert.equal(err.message, "Запрос c операцией вычитания не соответствует необходимому шаблону");
+                    done();
                 });
         });
 
+        it('test4 long query, fix bug in mult multiplication_transformation', function(done) {
+            var query = new RelationalAlgebraQuery({
+                title: "test",
+                alias: "Кинотеатры AS Z, ФильмыКинотеатры AS X, ФильмыКинотеатры AS Y, ФильмыКинотеатры AS U",
+                target_list: "Z.*",
+                query_body: '(((((Y*Z*A))*B)[Y.ИдКинотеатра=Z.ИдКинотеатраANDY.ИдФильма<>Z.ИдФильмаANDY.ИдКинотеатра=A.ИдКинотеатраANDY.ИдФильма<>A.ИдФильмаANDZ.ИдФильма<>A.ИдФильмаANDY.ИдКинотеатра=B.ИдКинотеатраANDY.ИдФильма<>B.ИдФильмаANDA.ИдФильма<>B.ИдФильмаANDZ.ИдФильма<>B.ИдФильма])[B.ИдКинотеатра=X.ИдКинотеатра]X)'
+            });
 
+            query.convert()
+                .then(function(res) {
+
+                    assert.equal(query.query_body, "((Y*Z*A*B*X)[Y.ИдКинотеатра=Z.ИдКинотеатраANDY.ИдФильма<>Z.ИдФильмаANDY.ИдКинотеатра=A.ИдКинотеатраANDY.ИдФильма<>A.ИдФильмаANDZ.ИдФильма<>A.ИдФильмаANDY.ИдКинотеатра=B.ИдКинотеатраANDY.ИдФильма<>B.ИдФильмаANDA.ИдФильма<>B.ИдФильмаANDZ.ИдФильма<>B.ИдФильмаANDB.ИдКинотеатра=X.ИдКинотеатра])");
+
+                    done();
+                }).catch(function(err) {
+                    done(err);
+                });
+        });
 
     });
 
